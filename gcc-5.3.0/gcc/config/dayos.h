ce627299fd2a1056874cea22d77cf4c83ff0fc75
Yannick Pflanzer <yp1995@outlook.com>
2015-12-12 12:47:04 +0100

    Initial commit

---
+++ gcc-5.3.0/gcc/config/dayos.h
@@ -0,0 +1,26 @@
+/* Useful if you wish to make target-specific gcc changes. */
+#undef TARGET_DAYOS
+#define TARGET_DAYOS 1
+ 
+/* Default arguments you want when running your
+   i686-myos-gcc/x86_64-myos-gcc toolchain */
+#define LIB_SPEC "-ldayos" // -lg -lm" /* link against C standard libraries */
+                               /* modify this based on your needs */
+ 
+/* Don't automatically add extern "C" { } around header files. */
+#undef  NO_IMPLICIT_EXTERN_C
+#define NO_IMPLICIT_EXTERN_C 1
+ 
+/* Additional predefined macros. */
+#undef TARGET_OS_CPP_BUILTINS
+#define TARGET_OS_CPP_BUILTINS()      \
+  do {                                \
+    builtin_define ("__dayos__");      \
+    builtin_define ("__unix__");      \
+    builtin_assert ("system=dayos");   \
+    builtin_assert ("system=unix");   \
+    builtin_assert ("system=posix");   \
+  } while(0);
+  
+//#undef STANDARD_INCLUDE_DIR
+//#define STANDARD_INCLUDE_DIR "/include"
